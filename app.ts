aef3d02789026e6edd97878c554aac667515216b
lockdoor <lockdoor@gmail.com>
2022-07-18 14:43:55 +0700

    first commit

--- app.ts
+++ app.ts
@@ -1,3 +1,5 @@
 let text:string
 text = 'Hello World'
+let text2:string = 'hello lockdoor'
 console.log(text)
+console.log(text2)
---
+++ out/app.js
@@ -0,0 +1,6 @@
+var text;
+text = 'Hello World';
+var text2 = 'hello lockdoor';
+console.log(text);
+console.log(text2);
+//# sourceMappingURL=app.js.map
