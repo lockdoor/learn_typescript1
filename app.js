50c877a17615e2e849b7de28dabe6c2957d23f3b
lockdoor <lockdoor@gmail.com>
2022-07-18 16:10:00 +0700

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 //Homework (Template String Part 1)
+//1 ทดลองสร้าง string อะไรก็ได้มา 1 ตัวและทดลองใช้ function length ที่หาความยาวของ string
+var text = 'careervio';
+console.log(text.length); //9
+//2 นำ string จาก ข้อที่ 1 มา แล้วใช้ function charAt() เพื่อเข้าถึง element แต่ละตัวของ string
+console.log(text.charAt(1)); //a
+//3 เช่นเดียวกับข้อที่ 2 ลองใช้ในรูปแบบที่เป็น [] (index) เพื่อเข้าถึง element ของ string เช่นกัน
+console.log(text[1]); //a
+//4 ลองเปรียบเทียบดูว่า charAt() กับ [](index) มีการทำงานที่เหมือนกันหรือไม่ และมีข้อดีหรือข้อเสียอย่างไร
+//string.charAt(10) จะ return เป็น string ว่าง
+console.log(text.charAt(10)); //
+//string[10] จะ return undefined
+console.log(text[10]); //undefined
+//5 จงนำเอา string 'hello' และ 'world' มาต่อกัน
+console.log('hello' + 'world');
+//Homework (Template String Part 2)
 //1 ทดลองเปลี่ยน string 'microsoft excel' ให้เป็นตัวใหญ่ทั้งหมด
-var text = 'microsoft excel';
-console.log(text.toUpperCase());
+text = 'microsoft excel';
+console.log(text.toUpperCase()); //MICROSOFT EXCEL
 //2 ทดลองเปลี่ยน string 'GOOGLE AND APPLE' ให้เป็นตัวเล็กทั้งหมด
 text = 'GOOGLE AND APPLE';
-console.log(text.toLowerCase());
+console.log(text.toLowerCase()); //google and apple
 //3 ทดลองเปลี่ยน string 'hello world' โดยที่ให้ตัวอักษร h และ w เป็นตัวใหญ่
 text = 'hello world';
-console.log(text.replace(/h|w/gi, function (x) { return x.toUpperCase(); }));
+console.log(text.replace(/h|w/gi, function (x) { return x.toUpperCase(); })); //Hello World
--- out/app.js
+++ out/app.js
@@ -1,6 +1,21 @@
 //Homework (Template String Part 1)
+//1 ทดลองสร้าง string อะไรก็ได้มา 1 ตัวและทดลองใช้ function length ที่หาความยาวของ string
+var text = 'careervio';
+console.log(text.length);
+//2 นำ string จาก ข้อที่ 1 มา แล้วใช้ function charAt() เพื่อเข้าถึง element แต่ละตัวของ string
+console.log(text.charAt(1));
+//3 เช่นเดียวกับข้อที่ 2 ลองใช้ในรูปแบบที่เป็น [] (index) เพื่อเข้าถึง element ของ string เช่นกัน
+console.log(text[1]);
+//4 ลองเปรียบเทียบดูว่า charAt() กับ [](index) มีการทำงานที่เหมือนกันหรือไม่ และมีข้อดีหรือข้อเสียอย่างไร
+//string.charAt(10) จะ return เป็น string ว่าง
+console.log(text.charAt(10));
+//string[10] จะ return un
+console.log(text[10]);
+//5 จงนำเอา string 'hello' และ 'world' มาต่อกัน
+console.log('hello' + 'world');
+//Homework (Template String Part 2)
 //1 ทดลองเปลี่ยน string 'microsoft excel' ให้เป็นตัวใหญ่ทั้งหมด
-var text = 'microsoft excel';
+text = 'microsoft excel';
 console.log(text.toUpperCase());
 //2 ทดลองเปลี่ยน string 'GOOGLE AND APPLE' ให้เป็นตัวเล็กทั้งหมด
 text = 'GOOGLE AND APPLE';
