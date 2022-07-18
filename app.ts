a0a18fda2c7b3da27937e9350ed9e9b9c555a573
lockdoor <lockdoor@gmail.com>
2022-07-18 15:35:49 +0700

    first commit

--- app.ts
+++ app.ts
@@ -1,5 +1,12 @@
-let text:string
-text = 'Hello World'
-let text2:string = 'hello lockdoor'
-console.log(text)
-console.log(text2)
+//Homework (Template String Part 1)
+//1 ทดลองเปลี่ยน string 'microsoft excel' ให้เป็นตัวใหญ่ทั้งหมด
+let text:string = 'microsoft excel'
+console.log(text.toUpperCase())
+
+//2 ทดลองเปลี่ยน string 'GOOGLE AND APPLE' ให้เป็นตัวเล็กทั้งหมด
+text = 'GOOGLE AND APPLE'
+console.log(text.toLowerCase())
+
+//3 ทดลองเปลี่ยน string 'hello world' โดยที่ให้ตัวอักษร h และ w เป็นตัวใหญ่
+text = 'hello world'
+console.log(text.replace(/h|w/gi, (x)=>x.toUpperCase()))
--- out/app.js
+++ out/app.js
@@ -1,6 +1,11 @@
-var text;
-text = 'Hello World';
-var text2 = 'hello lockdoor';
-console.log(text);
-console.log(text2);
+//Homework (Template String Part 1)
+//1 ทดลองเปลี่ยน string 'microsoft excel' ให้เป็นตัวใหญ่ทั้งหมด
+var text = 'microsoft excel';
+console.log(text.toUpperCase());
+//2 ทดลองเปลี่ยน string 'GOOGLE AND APPLE' ให้เป็นตัวเล็กทั้งหมด
+text = 'GOOGLE AND APPLE';
+console.log(text.toLowerCase());
+//3 ทดลองเปลี่ยน string 'hello world' โดยที่ให้ตัวอักษร h และ w เป็นตัวใหญ่
+text = 'hello world';
+console.log(text.replace(/h|w/gi, function (x) { return x.toUpperCase(); }));
 //# sourceMappingURL=app.js.map
